eae845bf8fa6d3090ca7f2b131d1cf3c44da631d
sajjad <banditofsocorstars@gmail.com>
2022-03-29 04:52:46 -0700

    app was finish

---
+++ main.js
@@ -0,0 +1,14 @@
+// convert to encode
+let convertToEnCode = () => {
+  // **
+  let enValue = document.getElementById("getText").value;
+  let enCodeString = window.btoa(enValue);
+  document.getElementById("show-encode").innerHTML = enCodeString;
+};
+
+// convert to decode
+let convertToDeCode = () => {
+  let deValue = document.getElementById("getText").value;
+  let deCodeString = window.atob(deValue);
+  document.getElementById("show-decode").innerHTML = deCodeString;
+};

2f25006bffb805bbf5e6693afb691b09688a7dd9
sajjad <banditofsocorstars@gmail.com>
2022-03-29 05:07:26 -0700

    add copy to clickboard

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ let convertToEnCode = () => {
   // **
   let enValue = document.getElementById("getText").value;
   let enCodeString = window.btoa(enValue);
-  document.getElementById("show-encode").innerHTML = enCodeString;
+  // document.getElementById("show-encode").innerHTML = enCodeString;
+  navigator.clipboard.writeText(enCodeString);
 };
 
 // convert to decode
 let convertToDeCode = () => {
   let deValue = document.getElementById("getText").value;
   let deCodeString = window.atob(deValue);
-  document.getElementById("show-decode").innerHTML = deCodeString;
+  // document.getElementById("show-decode").innerHTML = deCodeString;
+  navigator.clipboard.writeText(deCodeString);
 };
